7fa2ffc64f60e8dadeceb1be96e160ced2d008b1
Diego Cardoso Nunes <diegocar.nunes@gmail.com>
2023-12-14 18:57:39 -0300

    Change logout URL

--- server/config/settings.py
+++ server/config/settings.py
@@ -106,6 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 AUTH_USER_MODEL = "accounts.User"
 LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
