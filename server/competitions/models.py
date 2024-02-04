fefb6c3f201c7d7eae3f55da55237282470042a0
Rafael Lacerda Busatta <lacerdabusatta@gmail.com>
2024-02-04 18:42:24 -0300

    Criação do formulário estilizado na página de eventos para busca - versão beta;

--- server/competitions/models.py
+++ server/competitions/models.py
@@ -24,26 +24,6 @@ class CompetitionManager(models.Manager):
 
 import django_filters
 
-class CompetitionFilter(django_filters.FilterSet):
-    modality = django_filters.CharFilter(lookup_expr='icontains', label='Modalidade')
-
-    subscription_price = django_filters.NumberFilter()
-    subscription_price__lt = django_filters.NumberFilter(field_name='subscription_price', lookup_expr='lt')
-    subscription_price__gt = django_filters.NumberFilter(field_name='subscription_price', lookup_expr='gt')
-
-    max_slots = django_filters.NumberFilter()
-    max_slots__lt = django_filters.NumberFilter(field_name='max_slots', lookup_expr='lt')
-    max_slots__gt = django_filters.NumberFilter(field_name='max_slots', lookup_expr='gt')
-
-    city = django_filters.CharFilter(lookup_expr='icontains', label='Cidade')
-    state = django_filters.CharFilter(lookup_expr='icontains', label='Estado')
-
-
-    class Meta:
-        model = Competition
-        fields = ['datetime', 'city', 'state', 'subscription_price', 'max_slots']
-
-
 class Competition(models.Model):
     organizer = models.ForeignKey(
         User, on_delete=models.PROTECT, verbose_name="Organizador"
@@ -322,3 +302,22 @@ class OrganizerRequest(models.Model):
             .count()
         )
         return account_age.days > 30 and num_competitions >= 5
+    
+class CompetitionFilter(django_filters.FilterSet):
+    modality = django_filters.ModelChoiceFilter(queryset=Modality.objects.all(), label='Modalidade')
+
+    subscription_price = django_filters.NumberFilter()
+    subscription_price__lt = django_filters.NumberFilter(field_name='subscription_price', lookup_expr='lt')
+    subscription_price__gt = django_filters.NumberFilter(field_name='subscription_price', lookup_expr='gt')
+
+    max_slots = django_filters.NumberFilter()
+    max_slots__lt = django_filters.NumberFilter(field_name='max_slots', lookup_expr='lt')
+    max_slots__gt = django_filters.NumberFilter(field_name='max_slots', lookup_expr='gt')
+
+    city = django_filters.CharFilter(lookup_expr='icontains', label='Cidade')
+    state = django_filters.CharFilter(lookup_expr='icontains', label='Estado')
+
+
+    class Meta:
+        model = Competition
+        fields = ['datetime', 'city', 'state', 'subscription_price', 'max_slots']
